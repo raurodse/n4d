adfbb4a6784bc251d2cb28bad52906f75f96e80e
cless <hectorgh@gmail.com>
2021-01-28 11:00:28 +0100

    updated version number

--- setup_base.py
+++ setup_base.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == '__main__':
 	
 	setup(name='n4d-base',
-		version='0.908',
+		version='0.912',
 		description='N4d base library directory',
 		long_description="""""",
 		author='Hector Garcia Huerta',
