dd945d01c95970ccbe241f78c7892b6f2c3faa18
cless <hectorgh@gmail.com>
2021-05-10 11:14:18 +0200

    get_backup_name already in utils

--- server/core.py
+++ server/core.py
@@ -1097,12 +1097,5 @@ class Core:
 		
 	#def n4d_get_user
 	
-	def get_backup_name(self,plugin_name):
-		
-		timestamp=time.strftime("%d%m%Y_%H%M%S")
-		
-		return timestamp + "_" + plugin_name + ".tar.gz"
-		
-	#def get_backup_name	
 	
 #class Core
