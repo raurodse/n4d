950995bc3925aa7f0e52f24b6644040dbd7a65c4
cless <hectorgh@gmail.com>
2021-06-21 14:47:16 +0200

    making sure we are creating a ticket for a valid user

--- built-in/tickets/get_ticket.py
+++ built-in/tickets/get_ticket.py
@@ -3,12 +3,17 @@ import os
 
 def get_ticket(self,user,password):
 
+	UNKNOWN_USER_ERROR=-10
+
 	ret=self.validate_user(user,password)
 	if ret["status"]!=0:
 		return n4d.responses.build_authentication_failed_response()
 		
 	ticket=self.tickets_manager.get_ticket(user)
-	return n4d.responses.build_successful_call_response(ticket)
+	if ticket!=None:
+		return n4d.responses.build_successful_call_response(ticket)
+	else:
+		return n4d.responses.build_failed_call_response(UNKNOWN_USER_ERROR,"Unknown user error.")
 
 #def set_variable
 
