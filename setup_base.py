e91689a58e35236c8bc45d61a174316886003d47
cless <hectorgh@gmail.com>
2021-07-06 15:51:22 +0200

    version bump

--- setup_base.py
+++ setup_base.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == '__main__':
 	
 	setup(name='n4d-base',
-		version='0.918',
+		version='0.940',
 		description='N4d base library directory',
 		long_description="""""",
 		author='Hector Garcia Huerta',
