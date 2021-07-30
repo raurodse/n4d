817594f6a29299730f4b843b98ba53aa181aeca3
cless <hectorgh@gmail.com>
2021-07-30 13:40:34 +0200

    adding builtin methods to get_methods

--- built-in/get_methods.py
+++ built-in/get_methods.py
@@ -8,6 +8,10 @@ def get_methods(self,*args):
 	for x in core.plugin_manager.plugins:
 		if core.plugin_manager.plugins[x]["found"]:
 			ret[x]=core.plugin_manager.plugins[x]["methods"]
+			
+	ret["built-in"]={}
+	for method in core.BUILTIN_FUNCTIONS:
+		ret["built-in"][method]={}
 	
 	return n4d.responses.build_successful_call_response(ret)
 
