db254541927e54ce48f9e0de5b0c3af21a84eba5
cless <hectorgh@gmail.com>
2021-09-09 10:54:43 +0200

    adding network functions to built-in

---
+++ built-in/network/get_all_ips.py
@@ -0,0 +1,8 @@
+def get_all_ips(self):
+
+	#internal core function
+	ips=self.get_all_ips()
+	
+	return n4d.responses.build_successful_call_response(ips)
+
+#def get_all_ips
---
+++ built-in/network/get_ip_from_device.py
@@ -0,0 +1,6 @@
+def get_ip_from_device(self,dev):
+
+	#internal core function
+	return self.get_ip_from_device(dev)
+
+#def get_ip_from_device
