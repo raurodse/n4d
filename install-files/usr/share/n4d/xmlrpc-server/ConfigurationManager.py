cb439970ecfb8277059338ede2df7cd74186c335
cless <hectorgh@gmail.com>
2021-09-14 13:50:27 +0200

    making sure reading wrong conf file doesnt kill main process

--- install-files/usr/share/n4d/xmlrpc-server/ConfigurationManager.py
+++ install-files/usr/share/n4d/xmlrpc-server/ConfigurationManager.py
@@ -55,115 +55,120 @@ class ConfigurationManager:
 	
 	def load_plugin(self,file):
 
-		plugin=PluginInfo()
-		config = ConfigParser.ConfigParser()
-		config.optionxform=str
-		config.read(file)
-			
-		
-		if config.has_section("SETUP") and config.has_option("SETUP","type"):
-		
-			ok=True
-			
+		try:
+
+			plugin=PluginInfo()
+			config = ConfigParser.ConfigParser()
+			config.optionxform=str
+			config.read(file)
 				
-			try:
 			
-				if config.get("SETUP","type")=="python":
-					plugin.type="python"
-					if config.has_option("SETUP","path") and config.has_option("SETUP","class"):
-						plugin.path=self.python_path + config.get("SETUP","path")
-						plugin.class_name=config.get("SETUP","class")
-						if config.has_section("METHODS"):
-							options=config.options("METHODS")
-							for option in options:
-								tmp=config.get("METHODS",option)
-								tmp=tmp.replace(' ','')
-								perm_list=tmp.split(",")
-								
-								if option.find("(")==0:
-									first=option.find("(")+1
-									last=option.find(")")
-									secure_options=option[first:last].split(",")
-									option=option[last+1:]
-										
-									if "us" in secure_options:
-										plugin.user_secured_functions.append(option)
-									if "ip" in secure_options:
-										plugin.ip_secured_functions.append(option)
-											
+			if config.has_section("SETUP") and config.has_option("SETUP","type"):
+			
+				ok=True
+				
+					
+				try:
+				
+					if config.get("SETUP","type")=="python":
+						plugin.type="python"
+						if config.has_option("SETUP","path") and config.has_option("SETUP","class"):
+							plugin.path=self.python_path + config.get("SETUP","path")
+							plugin.class_name=config.get("SETUP","class")
+							if config.has_section("METHODS"):
+								options=config.options("METHODS")
+								for option in options:
+									tmp=config.get("METHODS",option)
+									tmp=tmp.replace(' ','')
+									perm_list=tmp.split(",")
 									
+									if option.find("(")==0:
+										first=option.find("(")+1
+										last=option.find(")")
+										secure_options=option[first:last].split(",")
+										option=option[last+1:]
+											
+										if "us" in secure_options:
+											plugin.user_secured_functions.append(option)
+										if "ip" in secure_options:
+											plugin.ip_secured_functions.append(option)
+												
 										
-								plugin.function[option]=perm_list
-						else:
-							ok=False
-					else:
-						ok=False
-						
-				if config.get("SETUP","type")=="binary":
-					plugin.type="binary"
-					if config.has_option("SETUP","path") and config.has_option("SETUP","class"):
-						plugin.path=self.bash_path + config.get("SETUP","path")
-						plugin.bin_name=config.get("SETUP","path")
-						plugin.class_name=config.get("SETUP","class")
-							
-														
-						if config.has_section("METHODS"):
-							plugin.standalone=False
-							options=config.options("METHODS")
-							for option in options:
-								tmp=config.get("METHODS",option)
-								tmp=tmp.replace(' ','')
-								perm_list=tmp.split(",")
-								plugin.function[option]=perm_list
-						else:
-							if config.has_option("SETUP","perms"):
-								tmp=config.get("SETUP","perms")
-								tmp=tmp.replace(' ','')
-								perm_list=tmp.split(",")
-								plugin.standalone=True
-								plugin.function[config.get("SETUP","path")]=perm_list
+											
+									plugin.function[option]=perm_list
 							else:
 								ok=False
-							
-					else:
-						
-						ok=False
-					
-				if config.get("SETUP","type")=="remote":
-					plugin.type="remote"
-					if config.has_option("SETUP","remoteip") and config.has_option("SETUP","order"):
-						plugin.remoteip=config.get("SETUP","remoteip")
-						plugin.order=config.get("SETUP","order")
-						plugin.functionremote=config.options("METHODS")
-					else:
-						ok=False
-					
-				if config.get("SETUP","type")=="perl":
-					plugin.type="perl"
-					if config.has_option("SETUP","path") and config.has_option("SETUP","class"):
-						plugin.path=self.perl_path + config.get("SETUP","path")
-						plugin.class_name=config.get("SETUP","class")
-						if config.has_section("METHODS"):
-							options=config.options("METHODS")
-							for option in options:
-								tmp=config.get("METHODS",option)
-								tmp=tmp.replace(' ','')
-								perm_list=tmp.split(",")
-								plugin.function[option]=perm_list
 						else:
 							ok=False
-					else:
-						ok=False
-
-				if ok:
-					return plugin
-				else:
-					return None
-
+							
+					if config.get("SETUP","type")=="binary":
+						plugin.type="binary"
+						if config.has_option("SETUP","path") and config.has_option("SETUP","class"):
+							plugin.path=self.bash_path + config.get("SETUP","path")
+							plugin.bin_name=config.get("SETUP","path")
+							plugin.class_name=config.get("SETUP","class")
+								
+															
+							if config.has_section("METHODS"):
+								plugin.standalone=False
+								options=config.options("METHODS")
+								for option in options:
+									tmp=config.get("METHODS",option)
+									tmp=tmp.replace(' ','')
+									perm_list=tmp.split(",")
+									plugin.function[option]=perm_list
+							else:
+								if config.has_option("SETUP","perms"):
+									tmp=config.get("SETUP","perms")
+									tmp=tmp.replace(' ','')
+									perm_list=tmp.split(",")
+									plugin.standalone=True
+									plugin.function[config.get("SETUP","path")]=perm_list
+								else:
+									ok=False
+								
+						else:
+							
+							ok=False
 						
-			except Exception as e:
-				print e
-				return None
+					if config.get("SETUP","type")=="remote":
+						plugin.type="remote"
+						if config.has_option("SETUP","remoteip") and config.has_option("SETUP","order"):
+							plugin.remoteip=config.get("SETUP","remoteip")
+							plugin.order=config.get("SETUP","order")
+							plugin.functionremote=config.options("METHODS")
+						else:
+							ok=False
+						
+					if config.get("SETUP","type")=="perl":
+						plugin.type="perl"
+						if config.has_option("SETUP","path") and config.has_option("SETUP","class"):
+							plugin.path=self.perl_path + config.get("SETUP","path")
+							plugin.class_name=config.get("SETUP","class")
+							if config.has_section("METHODS"):
+								options=config.options("METHODS")
+								for option in options:
+									tmp=config.get("METHODS",option)
+									tmp=tmp.replace(' ','')
+									perm_list=tmp.split(",")
+									plugin.function[option]=perm_list
+							else:
+								ok=False
+						else:
+							ok=False
+
+					if ok:
+						return plugin
+					else:
+						return None
+
+							
+				except Exception as e:
+					print e
+					return None
+				
+		except:
+			pass
 		
 	#def load_plugin
 
